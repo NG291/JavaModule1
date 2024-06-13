facea4386c1085d9a598e28cfeed5583986a3911
NG291 <you@example.com>
2024-06-13 10:33:56 +0700

    vision

---
+++ src/AccessModifier/Circle/Main.java
@@ -0,0 +1,8 @@
+package AccessModifier.Circle;
+
+public class Main {
+public static void main(String[] args) {
+    Circle circle = new Circle();
+   circle.toString();
+}
+}
---
+++ src/Inheritance/GeometricObject/Rectangle.java
@@ -0,0 +1,4 @@
+package Inheritance.GeometricObject;
+
+public class Rectangle {
+}
